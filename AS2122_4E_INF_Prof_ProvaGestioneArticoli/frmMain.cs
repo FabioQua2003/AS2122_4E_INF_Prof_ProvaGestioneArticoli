439b9c9fac53d2f9c68efc0fdea6964b33a62a54
FabioQua2003 <fabioqudrelli@gmail.com>
2022-03-16 10:28:53 +0100

    Ore 10:28 Consegna

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmMain.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmMain.cs
@@ -68,8 +68,8 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
             // TODO: (3) aggiungere visualizzazione dettaglio articolo nelle label
             // ... lblDescrizione.Text = articoli[index].Descrizione;
             lblDescrizione.Text = articoli[index].Descrizione;
-            lblUnitaMisura.Text = articoli[index].UnitaMisura;
-            lblPrezzo.Text = articoli[index].Prezzo.toString();
+            lblUnitaMisura.Text = articoli[index].UnitaDiMisura;
+            lblPrezzo.Text = articoli[index].Prezzo.ToString();
 
         }
 
