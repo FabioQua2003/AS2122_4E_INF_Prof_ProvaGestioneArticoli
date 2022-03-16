82e55e4561af5e36931394209c67b79f0439e271
FabioQua2003 <fabioqudrelli@gmail.com>
2022-03-16 09:44:51 +0100

    - (6) aggiungere property di sola lettura dei dati inseriti nella frmArticoli per l'utilizzo in frmMain

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmArticolo.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmArticolo.cs
@@ -16,9 +16,9 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
 
         // TODO: (5) aggiungere attributi privati dei dati inseriti nella frmArticoli
         // ...
-        string descrizione;
-        string unitaMisura;
-        double prezzo;
+        string descrizione { get; set; } 
+        string unitaMisura { get; set; }
+        double prezzo { get; set; }
 
         public DialogResult Status { get { return status; } }
 
